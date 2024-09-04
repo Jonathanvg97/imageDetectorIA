b4714402676667795a0619c07b7fd5fc76349e30
Jonathan Vanegas <jonathanvanegasg@hotmail.com>
2024-09-04 17:31:28 -0500

    Remove ignored files from repository

--- imageDetectorIABack/src/routes/huggingTranslateRoutes.js
+++
@@ -1,9 +0,0 @@
-import expres from 'express';
-import { translateTextController } from '../controllers/huggingTranslateController.js';
-
-const router = expres.Router();
-
-// Ruta para traducir texto
-router.post('/translate-text', translateTextController);
-
-export default router;
