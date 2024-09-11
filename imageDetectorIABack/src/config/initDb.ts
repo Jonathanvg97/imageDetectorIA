5132577f7a8a7ca634eebf2b44cf52113d770f6b
Jonathan <jonathanvanegasg@hotmail.com>
2024-09-10 22:24:39 -0500

    migrar a pg como base de datos y cliente

---
+++ imageDetectorIABack/src/config/initDb.ts
@@ -0,0 +1,39 @@
+import { Client } from "pg";
+import { envs } from "./envs";
+
+const client = new Client({
+  connectionString: envs.DATABASE_URL,
+  ssl: false,
+});
+
+const initDb = async () => {
+  try {
+    await client.connect();
+
+    // Crear tabla consumption si no existe
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS consumption (
+        clientIp TEXT PRIMARY KEY,
+        count INT NOT NULL DEFAULT 0
+      );
+    `);
+    // Crear tabla users si no existe
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        email TEXT NOT NULL UNIQUE,
+        name TEXT,
+        picture TEXT
+      );
+    `);
+
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.error("Error initializing database:", error);
+    process.exit(1); // Salir con error si algo sale mal
+  } finally {
+    await client.end();
+  }
+};
+
+initDb();
