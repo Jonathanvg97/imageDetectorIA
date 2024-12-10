bfce1939f3413a9a0a0fd0c098de3e7c91117c91
Jonathan <jonathanvanegasg@hotmail.com>
2024-12-09 21:09:25 -0500

    ajuste

--- imageDetectorIABack/src/server.ts
+++ imageDetectorIABack/src/server.ts
@@ -7,6 +7,11 @@ export const server = express();
 
 server.use(express.json());
 server.use(cookieParser());
-server.use(cors({ origin: envs.FRONTEND_BASE_URL }));
-server.use(express.urlencoded({ extended: true }));
+server.use(cors({ origin: "*" }));
 
+server.use((req, res, next) => {
+  res.setHeader("Referrer-Policy", "strict-origin-when-cross-origin");
+  next();
+});
+// server.use(cors({ origin: envs.FRONTEND_BASE_URL }));
+server.use(express.urlencoded({ extended: true }));
